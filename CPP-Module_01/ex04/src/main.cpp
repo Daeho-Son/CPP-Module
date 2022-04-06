9c313bfc5e97ccc01f3c31581da23f50f6eceb04
Sondho <thseogh135@gmail.com>
2022-04-06 15:12:58 +0900

    Implement replace()

--- CPP-Module_01/ex04/src/main.cpp
+++ CPP-Module_01/ex04/src/main.cpp
@@ -1,39 +1,65 @@
-#include "../include/main.hpp"
+#include "main.hpp"
 
-bool is_parameter_error(int argc, char** argv)
+static bool is_parameter_valid(int argc)
 {
-	std::string filename;
-	std::ifstream ifs;
-
 	if (argc != 4)
 	{
 		std::cout << "! Three parameters are required." << std::endl;
 		return (false);
 	}
-	filename = argv[1];
-	ifs.open(filename);
-	if (ifs.fail())
-	{
-		std::cout << "The file does not exist." << std::endl;
-		return (false);
-	}
 	return (true);
+}
+
+std::string replace_old_word_to_new_word(std::string old_string, std::string old_word, std::string new_word)
+{
+	std::string new_string = "";
+	std::string::size_type n = 0;
+	std::string prev_string = "";
+	std::string next_string = "";
+
+	for (int i = 0; i < static_cast<int>(old_string.size()); i++)
+	{
+		n = old_string.find(old_word, i);
+		if (n != std::string::npos)
+		{
+			std::cout << "old_string: " << old_string << std::endl;
+			prev_string = old_string.substr(0, n);
+			std::cout << "prev_string: " << prev_string << std::endl;
+			next_string = old_string.substr(n + old_word.size(), old_string.size());
+			std::cout << "next_sting: " << next_string << std::endl;
+			new_string = prev_string + new_word + next_string;
+			std::cout << "new_string: " << new_string << std::endl;
+			old_string = new_string;
+			i = prev_string.size() + new_word.size() - 1;
+			std::cout << "----------" << std::endl;
+		}
+	}
+	return (new_string);
 }
 
 int main(int argc, char** argv)
 {
-	std::ifstream input_file;
-	std::string filename;
+	std::ifstream i_file;
+	std::ofstream o_file;
+	std::string old_word;
+	std::string new_word;
 	std::string read_string;
+	std::string replaced_string;
 
-	if (is_parameter_error(argc, argv) == false)
-		exit(0);
-	filename = argv[1];
-	input_file.open(filename);
-	while (!input_file.eof())
+	if (is_parameter_valid(argc) == false)
+		return (0);
+	old_word = argv[2];
+	new_word = argv[3];
+	i_file = open_input_file(argv[1]);
+	o_file = open_output_file(argv[1]);
+	while (!i_file.eof())
 	{
-		getline(input_file, read_string);
-		std::cout << read_string << std::endl;
+		getline(i_file, read_string);
+		replaced_string = replace_old_word_to_new_word(read_string, old_word, new_word);
+		o_file << replaced_string << std::endl;
+		std::cout << std::endl;
 	}
+	i_file.close();
+	o_file.close();
 	return (0);
 }

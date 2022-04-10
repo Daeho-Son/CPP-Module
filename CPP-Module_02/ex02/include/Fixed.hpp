d37623eb2346358ac23621729eeb45adf87aa54c
Sondho <thseogh135@gmail.com>
2022-04-11 00:26:42 +0900

    Implement 02_ex02

--- CPP-Module_02/ex02/include/Fixed.hpp
+++ CPP-Module_02/ex02/include/Fixed.hpp
@@ -19,6 +19,28 @@ class Fixed
 
 		Fixed& operator=(const Fixed& fixed);
 
+		Fixed operator+(const Fixed& fixed);
+		Fixed operator-(const Fixed& fixed);
+		Fixed operator*(const Fixed& fixed);
+		Fixed operator/(const Fixed& fixed);
+
+		Fixed& operator++(void);
+		Fixed operator++(int);
+		Fixed& operator--(void);
+		Fixed operator--(int);
+
+		bool operator>(const Fixed& fixed) const;
+		bool operator<(const Fixed& fixed) const;
+		bool operator>=(const Fixed& fixed) const;
+		bool operator<=(const Fixed& fixed) const;
+		bool operator==(const Fixed& fixed) const;
+		bool operator!=(const Fixed& fixed) const;
+
+		static Fixed& min(Fixed& fixed_a, Fixed& fixed_b);
+		static const Fixed& min(const Fixed& fixed_a, const Fixed& fixed_b);
+		static Fixed& max(Fixed& fixed_a, Fixed& fixed_b);
+		static const Fixed& max(const Fixed& fixed_a, const Fixed& fixed_b);
+
 		int getRawBits(void) const;
 		void setRawBits(const int raw);
 		float toFloat(void) const;
--- CPP-Module_02/ex02/src/Fixed.cpp
+++ CPP-Module_02/ex02/src/Fixed.cpp
@@ -2,44 +2,149 @@
 
 Fixed::Fixed()
 {
-	std::cout << "Default constructor called" << std::endl;
+	// std::cout << "Default constructor called" << std::endl;
 	fixed_point_ = 0;
 }
 
 Fixed::Fixed(const int input_n)
 {
-	std::cout << "Int constructor called" << std::endl;
+	// std::cout << "Int constructor called" << std::endl;
 	fixed_point_ = input_n << fractional_bits_;
 }
 
 Fixed::Fixed(const float input_f)
 {
-	std::cout << "Float constructor called" << std::endl;
+	// std::cout << "Float constructor called" << std::endl;
 	fixed_point_ = roundf(input_f * (1 << fractional_bits_));
 }
 
 Fixed::Fixed(const Fixed& fixed)
 {
-	std::cout << "Copy constructor called" << std::endl;
+	// std::cout << "Copy constructor called" << std::endl;
 	*this = fixed;
 }
 
 Fixed::~Fixed()
 {
-	std::cout << "Destructor called" << std::endl;
-}
-
-Fixed& Fixed::operator=(const Fixed& fixed)
-{
-	std::cout << "Copy assignment operator called" << std::endl;
-	fixed_point_ = fixed.getRawBits();
-	return (*this);
+	// std::cout << "Destructor called" << std::endl;
 }
 
 std::ostream& operator<<(std::ostream& o_stream, const Fixed& fixed)
 {
 	std::cout << fixed.toFloat();
 	return (o_stream);
+}
+
+Fixed& Fixed::operator=(const Fixed& fixed)
+{
+	// std::cout << "Copy assignment operator called" << std::endl;
+	fixed_point_ = fixed.getRawBits();
+	return (*this);
+}
+
+Fixed Fixed::operator+(const Fixed& fixed)
+{
+	return (this->toFloat() + fixed.toFloat());
+}
+
+Fixed Fixed::operator-(const Fixed& fixed)
+{
+	return (this->toFloat() - fixed.toFloat());
+}
+
+Fixed Fixed::operator*(const Fixed& fixed)
+{
+	return (this->toFloat() * fixed.toFloat());
+}
+
+Fixed Fixed::operator/(const Fixed& fixed)
+{
+	fixed_point_ /= fixed.fixed_point_;
+	return (this->toFloat() / fixed.toFloat());
+}
+
+bool Fixed::operator>(const Fixed &fixed) const
+{
+	return (fixed_point_ > fixed.fixed_point_);
+}
+
+bool Fixed::operator<(const Fixed &fixed) const
+{
+	return (fixed_point_ < fixed.fixed_point_);
+}
+
+bool Fixed::operator>=(const Fixed &fixed) const
+{
+	return (fixed_point_ >= fixed.fixed_point_);
+}
+
+bool Fixed::operator<=(const Fixed &fixed) const
+{
+	return (fixed_point_ <= fixed.fixed_point_);
+}
+
+bool Fixed::operator==(const Fixed &fixed) const
+{
+	return (fixed_point_ == fixed.fixed_point_);
+}
+
+bool Fixed::operator!=(const Fixed &fixed) const
+{
+	return (fixed_point_ != fixed.fixed_point_);
+}
+
+Fixed& Fixed::operator++(void)
+{
+	fixed_point_++;
+	return (*this);
+}
+
+Fixed Fixed::operator++(int)
+{
+	Fixed temp = *this;
+	++*this;
+	return (temp);
+}
+
+Fixed& Fixed::operator--(void)
+{
+	fixed_point_--;
+	return (*this);
+}
+
+Fixed Fixed::operator--(int)
+{
+	Fixed temp = *this;
+	++*this;
+	return (temp);
+}
+
+Fixed& Fixed::min(Fixed& fixed_a, Fixed& fixed_b)
+{
+	if (fixed_a.fixed_point_ < fixed_b.fixed_point_)
+		return (fixed_a);
+	return (fixed_b);
+}
+
+const Fixed& Fixed::min(const Fixed& fixed_a, const Fixed& fixed_b)
+{
+	if (fixed_a.fixed_point_ < fixed_b.fixed_point_)
+		return (fixed_a);
+	return (fixed_b);
+}
+
+Fixed& Fixed::max(Fixed& fixed_a, Fixed& fixed_b)
+{
+	if (fixed_a.fixed_point_ > fixed_b.fixed_point_)
+		return (fixed_a);
+	return (fixed_b);
+}
+
+const Fixed& Fixed::max(const Fixed& fixed_a, const Fixed& fixed_b)
+{
+	if (fixed_a.fixed_point_ > fixed_b.fixed_point_)
+		return (fixed_a);
+	return (fixed_b);
 }
 
 int Fixed::getRawBits(void) const

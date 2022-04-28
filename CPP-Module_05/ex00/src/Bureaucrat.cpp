d31576172841fe80018aa0ca37de3e5d2d6e6683
Daeho Son <dason@c3r4s7.42seoul.kr>
2022-04-28 19:55:11 +0900

    Modify what()

--- CPP-Module_05/ex00/src/Bureaucrat.cpp
+++ CPP-Module_05/ex00/src/Bureaucrat.cpp
@@ -68,3 +68,13 @@ void Bureaucrat::upGrade()
 		throw GradeTooHighException();
 	grade_--;
 }
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Bureaucrat::GradeTooHighException()");
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Bureaucrat::GradeTooLowException()");
+}

a5c0476186fadfe3884d6a713c41d7778cdbdc06
Daeho Son <dason@c3r5s7.42seoul.kr>
2022-05-02 15:55:47 +0900

    Done 07_ex01

--- CPP-Module_07/ex01/include/iter.hpp
+++ CPP-Module_07/ex01/include/iter.hpp
@@ -2,9 +2,25 @@
 # define ITER_HPP
 
 template<typename T>
-void iter(T* address, T len, )
+void iter(T* arr, int len, void (*f)(T))
 {
+	for (int i = 0; i < len; i++)
+	{
+		f(arr[i]);
+	}
+	std::cout << std::endl;
+}
 
+template<typename T>
+void print(T x)
+{
+	std::cout << x << " ";
+}
+
+template<typename T>
+void plus_one(T x)
+{
+	std::cout << static_cast<T>(x + 1) << " ";
 }
 
 #endif
--- CPP-Module_07/ex01/src/main.cpp
+++ CPP-Module_07/ex01/src/main.cpp
@@ -3,5 +3,40 @@
 
 int main(void)
 {
+	{
+		std::cout << "===== int =====" << std::endl;
+		int i[5] = {1, 2, 3, 4, 5};
+		std::cout << "# print()" << std::endl;
+		iter(i, 5, print);
+		std::cout << "# plus_one()" << std::endl;
+		iter(i, 5, plus_one);
+	}
+	std::cout << std::endl << std::endl;
+	{
+		std::cout << "===== char =====" << std::endl;
+		char c[5] = {'a', 'b', 'c', 'd', 'e'};
+		std::cout << "# print()" << std::endl;
+		iter(c, 5, print);
+		std::cout << "# plus_one()" << std::endl;
+		iter(c, 5, plus_one);
+	}
+	std::cout << std::endl << std::endl;
+	{
+		std::cout << "===== float =====" << std::endl;
+		float f[5] = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f};
+		std::cout << "# print()" << std::endl;
+		iter(f, 5, print);
+		std::cout << "# plus_one()" << std::endl;
+		iter(f, 5, plus_one);
+	}
+	std::cout << std::endl << std::endl;
+	{
+		std::cout << "===== double =====" << std::endl;
+		double d[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
+		std::cout << "# print()" << std::endl;
+		iter(d, 5, print);
+		std::cout << "# plus_one()" << std::endl;
+		iter(d, 5, plus_one);
+	}
 	return (0);
 }
